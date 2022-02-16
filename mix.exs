56a3c595dd2aa635956afbf6c0dfc08ee2c19869
Eksperimental <eksperimental@autistici.org>
2022-02-16 03:05:48 -0500

    Add Credo

--- mix.exs
+++ mix.exs
@@ -31,6 +31,7 @@ defmodule SampleProject.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:credo, "~> 1.6", only: [:dev], runtime: false},
       {:ex_doc, "~> 0.28", only: :dev, runtime: false}
     ]
   end
